b5aeb7e39f2c3bbbf685ac30b2b071a47ccdb9e6
Matt-Murungi <murungima@gmail.com>
2023-07-11 17:53:31 +0300

    chore: configure celery and flower

--- authors_api/settings/base.py
+++ authors_api/settings/base.py
@@ -43,6 +43,7 @@ THIRD_PARTY_APPS = [
     "phonenumber_field",
     "drf_yasg",
     "corsheaders",
+    "djcelery_email"
     # "djcelery_email",
     # "djoser"
     # "rest_framework_simplejwt",
@@ -175,6 +176,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_URLS_REGEX = r"^/api/.*$"
 
 AUTH_USER_MODEL = "users.User"
+
+CELERY_BROKER_URL = env("CELERY_BROKER")
+CELERY_RESULT_BACKEND = CELERY_BROKER_URL
+CELERY_ACCEPT_CONTENT = ["json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
+CELERY_RESULT_BACKEND_MAX_RETRIES = 10
+CELERY_TASK_SEND_SENT_EVENT = True
+
+if USE_TZ:
+    CELERY_TIMEZONE = TIME_ZONE
+
 
 LOGGING = {
     "version": 1,
--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -9,3 +9,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
+EMAIL_PORT = env("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = "support@murungima.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Tiny Medium"
