86c4c499ff0037d70fbc83ced42f8861c286573b
Matt-Murungi <murungima@gmail.com>
2023-07-13 09:09:37 +0300

    feat: add profile

--- authors_api/settings/local.py
+++ authors_api/settings/local.py
@@ -13,6 +13,6 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost:8080']
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = env("EMAIL_HOST", default="mailhog")
 EMAIL_PORT = env("EMAIL_PORT")
-DEFAULT_FROM_EMAIL = "support@murungima.com"
+DEFAULT_FROM_EMAIL = "support@example.com"
 DOMAIN = env("DOMAIN")
 SITE_NAME = "Tiny Medium"
--- authors_api/urls.py
+++ authors_api/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from dj_rest_auth.views import PasswordResetConfirmView
+from core_apps.users.views import CustomUserDetailsView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -11,17 +13,23 @@ schema_view = get_schema_view(
         default_version="v1",
         description="API endpoints for Tiny Medium",
         contact=openapi.Contact(email="murungima@gmail.com"),
-        license=openapi.License(name="MIT License")
+        license=openapi.License(name="MIT License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
-
 )
 
 urlpatterns = [
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0)),
     path(settings.ADMIN_URL, admin.site.urls),
-
+    path("api/v1/auth/user/", CustomUserDetailsView.as_view(), name="user-details"),
+    path("api/v1/auth/", include("dj_rest_auth.urls")),
+    path("api/v1/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path(
+        "api/v1/auth/password/reset/confirm/<uidb64>/<token>/",
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
 ]
 
 admin.site.site_header = "Tiny Medium API Admin"
